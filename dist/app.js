296762ad4ac7f8c38064ea39bca72cedfcb1b187
Ndekocode <arickbulakali@gmail.com>
2022-12-13 11:31:28 +0200

    add basic type

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,17 @@
-var b = 0;
+"use strict";
+const a = "Hello world";
+const n = 0;
+const b = false;
+const d = null;
+const arr = ['lol', 'Bum'];
+const user = { username: '', firstname: "", lastname: '' };
+const username = [];
+const date = new Date();
+/*
+let i:number = 0;
+const compter = document.querySelector('#compter');
+const increment = (e) =>{
+    i++;
+    compter.querySelector('span').innerText =i.toString();
+}
+compter.addEventListener('click',increment); */ 
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-const b:number = 0;
+const a:string = "Hello world";
+const n:number = 0;
+const b:boolean = false;
+const d:null = null;
+const arr:string[] = ['lol','Bum'];
+const user:{username:string,firstname:string,lastname:string,[key:string]:string} = {username:'',firstname:"",lastname:''};
+const username:object[] =  [];
+const date:Date = new Date();
+/* 
+let i:number = 0;
+const compter = document.querySelector('#compter');
+const increment = (e) =>{
+    i++;
+    compter.querySelector('span').innerText =i.toString();
+}
+compter.addEventListener('click',increment); */
